257c470a7d0d9548354b4b2bad0466db6c104d11
ericb2020 <ebrauer83@gmail.com>
2021-02-12 09:45:18 -0600

    initial react boilerplate in place

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      user: {
+        name: "Casey Harding",
+        username: "CRHarding",
+        image: "https://static.wikia.nocookie.net/ssb/images/1/19/Link_-_Super_Smash_Bros._Ultimate.png/revision/latest/scale-to-width-down/1000?cb=20190612145102",
+        friendList: []
+      },
+      apiDataLoaded: false,
+      potentialFriends: []
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>CaseyBook</h1>
+      </div>
+    );
+  }
+}
 export default App;
